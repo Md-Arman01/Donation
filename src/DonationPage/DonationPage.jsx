3898440116bcae70ebce6e7ee2adc0aa8d2979f9
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2023-09-26 00:09:24 +0600

    updated see all button in donation page

--- src/DonationPage/DonationPage.jsx
+++ src/DonationPage/DonationPage.jsx
@@ -6,7 +6,7 @@ const DonationPage = () => {
 
     const [cards, setCards] = useState([])
     const [noDonate, setNoDonate] = useState()
-    const [isShow, setIsShow] = useState(false)
+    const [isShow, setIsShow] = useState(4)
     
     useEffect(()=>{
         const getCards = JSON.parse(localStorage.getItem('card'))
@@ -24,17 +24,18 @@ const DonationPage = () => {
             {
                 noDonate? <p className="flex justify-center items-center h-[400px] text-3xl font-semibold">{noDonate}</p> : <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                 {
-                    isShow? cards?.map((card, index) => <DonateCard key={index} card={card}></DonateCard>)
-                    :
-                    cards?.slice(0, 4).map((card, index) => <DonateCard key={index} card={card}></DonateCard>)
+                    cards?.slice(0, isShow).map((card, index) => <DonateCard key={index} card={card}></DonateCard>)
+                    
                 }
             </div>
             }
             
             {
                 cards.length > 4 &&
-            <div onClick={()=> setIsShow(!isShow)} className="flex justify-center my-7">
-                <button className="btn normal-case bg-[#009444] hover:bg-[#48a875] text-white w-40">{isShow? "See Less" : "See All"}</button>
+            <div className={isShow === cards.length && 'hidden'}>
+                <div className="flex justify-center my-7">
+                <button onClick={() => setIsShow(cards.length)} className="btn normal-case bg-[#009444] hover:bg-[#48a875] text-white w-40">See All</button>
+                </div>
             </div>
             }
 
